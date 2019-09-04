7ac8c98ca55894f65b6937a5429eee87f7d1f462
Andrés González <gandresto@outlook.com>
2019-09-04 02:04:46 -0500

    Segmentación funcionando.

--- Práctica1/p1_segmentacion.py
+++ Práctica1/p1_segmentacion.py
@@ -1,11 +1,14 @@
 #%%
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from threading import Semaphore
 #import scipy.io as spio
 #import matplotlib.pyplot as plt
 #%%
 # Usar region growing para obtener la región deseada
 def obtener_8_vecinos(x, y, shape):
+    #print('Onteniendo vecinos...')
     out = []
     maxx = shape[1]-1
     maxy = shape[0]-1
@@ -49,16 +52,19 @@ def obtener_8_vecinos(x, y, shape):
     outx = min(max(x+1,0),maxx)
     outy = min(max(y+1,0),maxy)
     out.append((outx,outy))
-
+    
+    #print('Vecinos obtenidos: ', out)
     return out
 
 def region_growing(img, seed):
+    print('Inicia el proceso...')
     list = []
     outimg = np.zeros_like(img)
     list.append((seed[0], seed[1]))
     processed = []
     while(len(list) > 0):
         pix = list[0]
+        #print('Analizando pixel: ', pix)
         outimg[pix[0], pix[1]] = 255
         for coord in obtener_8_vecinos(pix[0], pix[1], img.shape):
             if img[coord[0], coord[1]] != 0:
@@ -69,46 +75,46 @@ def region_growing(img, seed):
         list.pop(0)
         #cv2.imshow("progress",outimg)
         #cv2.waitKey(1)
+    print('Termina el proceso...')
     return outimg
 
-def on_mouse(event, x, y, flags, params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print('Seed: ' + str(x) + ', ' + str(y), img_bin[y,x])
-        clicks.append((y,x))
+#%%
+def onmouseclick(event):
+    if(event.inaxes == ax1):
+        #s.acquire()
+        seed = [np.round(event.ydata).astype(np.int), np.round(event.xdata).astype(np.int)]
+        print(seed)
+        img_rg = region_growing(img_bin, seed)
+        #ax2.imshow(np.random.randint(low=0, high=255, size=img_bin.shape, dtype=np.uint8))
+        ax2.imshow(img_rg, cmap='gray')
+        fig1.canvas.draw()
+        #s.release()
 
 #%%
+#s = Semaphore(1)
 img_bin = cv2.imread('./imagen_preprocesada.bmp', 0)
-clicks = []
-cv2.namedWindow('Input',cv2.WINDOW_NORMAL)
-cv2.setMouseCallback('Input', on_mouse, 0, )
-cv2.imshow('Input', img_bin)
-cv2.waitKey()
-seed = clicks[-1]
-out = region_growing(img_bin, seed)
-cv2.imshow('Region Growing', out)
-cv2.waitKey()
-cv2.destroyAllWindows()
+fig1 = plt.figure(figsize=(10, 7))
+fig1.canvas.mpl_connect('button_press_event', onmouseclick)
 
-# #%%
-# # Obtener la plantilla
-# template = cv2.imread('./template2.png')
-# template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-# plt.title('Template')
-# plt.imshow(template, cmap='gray')
-# plt.show()
+ax1 = fig1.add_subplot(121)
+ax1.set_title('Selecciona un área para medirla')
+ax1.imshow(img_bin, cmap='gray')
+ax1.axis('off')
 
-# #%%
-# result = cv2.matchTemplate(image = img_bin, templ = template, method=cv2.TM_CCOEFF_NORMED)
-# result = np.abs(result)**3
-# val, result = cv2.threshold(result, 0.01, 0, cv2.THRESH_TOZERO)
-# result8 = cv2.normalize(result,None,0,255,cv2.NORM_MINMAX,cv2.CV_8U)
-# plt.imshow(result8, cmap='gray')
+ax2 = fig1.add_subplot(122)
+ax2.set_title('Resultado')
+ax2.axis('off')
+ax2.imshow(img_bin, cmap='gray')
 
-#%%
-""" 
-# Suavizar bordes
-img_suav = cv2.GaussianBlur(img_eq,(3,3),0)
-plt.title('Imagen suavizada')
-plt.imshow(img_suav, cmap='gray')
+textstr = '\n'.join((
+    'Ancho: %d mm',
+    'Alto: %d mm',
+    'Área: %d mm\u00b2'))
 
-"""
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+
+# place a text box in upper left in axes coords
+ax2.text(0.05, 0.95, textstr, transform=ax2.transAxes, fontsize=12,
+        verticalalignment='top', bbox=props)
+
+plt.show()
